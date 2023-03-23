a038b6ea543ae87b521d8d26c0c93c0f7ecc82d6
jbowdle <johnwbowdle@gmail.com>
2023-03-22 19:07:12 -0500

    Fixed merge conflicts, made weather functions update index.html

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 const tempDisplay = document.querySelector("#temperature-display");
 const precipDisplay = document.querySelector("#precip-display");
 const windspeedDisplay = document.querySelector("#windspeed-display");
+const homeLogo = document.querySelector("#home-team-logo");
+const homeName = document.querySelector("#home-team-name");
+const awayLogo = document.querySelector("#away-team-logo");
+const awayName = document.querySelector("#away-team-name");
+const stadiumPic = document.querySelector("#stadium-pic");
+const stadiumName = document.querySelector("#stadium-name");
+const stadiumLocation = document.querySelector("#stadium-location");
 
 let matchLocation = {
     latitude: "",
@@ -42,4 +49,70 @@ const setWeatherDisplay = function(cityName, matchDate) {
         });
 }
 
+const getStadiumInfo = function(venueName) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3b05f88e7amsh439ba845456b31bp1767c4jsn41bc2d52d800',
+            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
 
+    // Sets stadium name, stadium picture, and location
+    fetch(`https://api-football-v1.p.rapidapi.com/v3/venues?name=${venueName}`, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const stadiumData = data.response[0];
+
+            stadiumPic.setAttribute("src", stadiumData.image);
+            stadiumName.textContent = venueName;
+            stadiumLocation.textContent = `${stadiumData.address}, ${stadiumData.city}`;
+        })
+        .catch(err => console.error(err));
+}
+
+const getUpcomingMatch = function() {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '3b05f88e7amsh439ba845456b31bp1767c4jsn41bc2d52d800',
+            'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+    };
+
+    // Sets name and logo for home and away teams
+    fetch(`https://api-football-v1.p.rapidapi.com/v3/fixtures?league=253&next=1`, options)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            // debug
+            console.log(data);
+
+            const teamData = data.response[0].teams;
+
+            homeLogo.setAttribute("src", teamData.home.logo);
+            homeName.textContent = teamData.home.name;
+            awayLogo.setAttribute("src", teamData.away.logo);
+            awayName.textContent = teamData.away.name;
+
+            const venueName = data.response[0].fixture.venue.name;
+
+            // sets stadium pic and name using venue name
+            getStadiumInfo(venueName);
+
+            const cityData = data.response[0].fixture.venue.city
+
+            const splitCityName = cityData.split(",")
+
+            const cityName = splitCityName[0];
+            const matchDate = data.response[0].fixture.date.slice(0, 10);
+
+            setWeatherDisplay(cityName, matchDate);
+        })
+        .catch(err => console.error(err));
+}
+
+getUpcomingMatch();

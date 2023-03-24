8ba99b8ff5fbc44042ea00f192a19691679868ea
blackshane <122633142+blackshane@users.noreply.github.com>
2023-03-24 17:41:32 -0500

    Map works when clicking on stadium

--- assets/directionsScript.js
+++ assets/directionsScript.js
@@ -28,7 +28,7 @@ function initMap(latitude, longitude) {
     Stadium('City Park STL', 38.6312, -90.2105),
     Stadium('Mercedes-Benz Stadium ATL', 33.755489, -84.401993),
     Stadium('Soldier Field CHI', 41.862366, -87.617256),
-    Stadium('Lower.com Field CBUS', 39.9685, -83.0171),
+    Stadium('Lower.com Field COL', 39.9685, -83.0171),
     Stadium('Shell Energy Stadium HUS', 29.7522, -95.3523),
     Stadium('Saputo Stadium MON', 45.562295, -73.553032),
     Stadium('Red Bull Arena NYC', 40.736844, -74.150236),
@@ -41,22 +41,22 @@ function initMap(latitude, longitude) {
     Stadium('Audi Field DC', 38.868851, -77.012894 ),
     Stadium("Children's Mercy Park KC", 39.12217, -94.8238),
     Stadium('DRV PNK Stadium MIA', 26.193287, -80.160629 ),
-    Stadium('GEODIS Park NAS', 36.130257 , -86.765907),
+    Stadium('GEODIS Park NASH', 36.130257 , -86.765907),
     Stadium('Yankee Stadium NYC', 40.829659, -73.926186),
     Stadium('Providence Park POR', 45.5214896604, -122.691963152 ),
     Stadium('Lumen Field SEA', 47.595211 , -122.331627),
     Stadium('BC Place VAN', 49.276750, -123.111999),
     Stadium('Bank of America Stadium CLT', 35.225845, -80.853607),
-    Stadium('Dick’s Sporting Goods Park DEN', 39.805691 , -104.891891 ),
+    Stadium("Dick's Sporting Goods Park DEN", 39.805691 , -104.891891 ),
     Stadium('Toyota Stadium DAL', 33.154320, -96.835426),
     Stadium('Dignity Health Sports Park LA', 33.864204 , -118.261177 ),
     Stadium('Allianz Field MIN', 44.9510554, -93.1607761 ),
     Stadium('Gillette Stadium NE', 42.0878 , -71.2587),
     Stadium('Exploria Stadium ORL', 28.541085, -81.389142),
     Stadium('America First Field SLC', 40.5699467494479, -111.8927205244),
-   
   )
   
+
   const stadiumListEl = document.getElementById('stadiumList')
 
   function loadStadiumList() {
@@ -68,18 +68,18 @@ function initMap(latitude, longitude) {
     }
   }
 
-
-  function handleStadiumClick(stadiumDivEl) {
-    let stadiumIndex = stadiumDivEl.getAttribute('data-stadium-index')
-    console.log(stadiumIndex) 
-
+  stadiumListEl.onclick = function(e) {
+    let stadiumIndex = e.target.getAttribute('data-stadium-index')
+    if (!stadiumIndex) {
+      return
+    }
+    
+    // e.target IS stadium
+    renderMap(e.target);
     let stadium = stadiums[stadiumIndex]
-    console.log(stadium)
-
     initMap(stadium.latitude, stadium.longitude)
   }
 
 
-  loadStadiumList()
 
- 
+  loadStadiumList()
